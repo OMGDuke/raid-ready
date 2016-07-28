ccfe73a93eaa49e66df11ab087cf1b833acc757b
OMGDuke <lex1989@gmail.com>
2016-07-28 19:54:59 +0100

    Added some input fields for name and server

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -1,25 +1,32 @@
 describe("raidReadyApp", function () {
+  beforeEach(function() {
+    browser.get('/');
+  });
 
   describe("headings and titles", function() {
     it("should get home page title", function() {
-      browser.get('/');
       expect(browser.getTitle()).toEqual("Raid Ready");
     });
 
     it('should have the app title on the page', function () {
-      browser.get('/');
       expect($('#header').getText()).toMatch('Raid Ready');
     });
   });
 
   describe("input form", function() {
+    it('should have a Server name field', function() {
+      expect($('#server-input').isPresent()).toBeTruthy();
+    });
+
+    it('should have a Guild name field', function() {
+      expect($('#guild-input').isPresent()).toBeTruthy();
+    });
+
     it('should have a Check button', function() {
-      browser.get('/');
       expect($('#check-btn').getText()).toMatch('Check my Guild');
     });
 
     xit('Check button should take you to the new url', function () {
-      browser.get('/');
       $('#check-btn').click();
       expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/check');
     });
