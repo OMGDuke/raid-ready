0584433c61454b7287aaed9d9d23f74821b3cdd5
OMGDuke <lex1989@gmail.com>
2016-07-28 22:44:00 +0100

    Implemented ui.router

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,13 @@
-var raidReadyApp = angular.module('raidReadyApp', []);
+var raidReadyApp = angular.module('raidReadyApp', ['ui.router']);
+
+raidReadyApp.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "views/home.html",
+      // controller: 'YourCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/');
+});
